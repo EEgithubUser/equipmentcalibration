fe94a109ea7e4224ac87b7dc62f31bd7f2df3b2e
Kevin <kevchan129@gmail.com>
2023-02-16 01:13:25 -0800

    style(format): clean script using class and methods

--- autoCal.py
+++ autoCal.py
@@ -6,37 +6,54 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-file = 'config.ini'
-config = ConfigParser()
-config.read(file)
 
-s = Service(config['service']['address'])
-driver = webdriver.Chrome(service = s)
-driver.get("https://www.pmi-link.com/login.php")
+webpage = "https://www.pmi-link.com/login.php"
 
-driver.find_element(By.CSS_SELECTOR,"input[id='user']").send_keys(config['account']['username'])
-driver.find_element(By.CSS_SELECTOR,"input[id='pass']").send_keys(config['account']['password'])
-driver.find_element(By.CSS_SELECTOR,"input[type='submit']").click()
-driver.find_element(By.XPATH,"(//a[@class='dropdown-toggle'])[2]").click()
-driver.find_element(By.XPATH,"(//a[@title='Create Shipper'])[1]").click()
-driver.find_element(By.XPATH,"(//input[@class='field'])[3]").click()
-wait = WebDriverWait(driver,20)
-time.sleep(10)
-wait.until(EC.element_to_be_clickable((By.XPATH,"(//img[@alt='next day pickup'])"))).click()
-windowsOpened = driver.window_handles
-driver.switch_to.window(windowsOpened[1])
-driver.find_element(By.CSS_SELECTOR,"select[id='shipTo']").click()
-driver.find_element(By.CSS_SELECTOR,"option[value='2']").click()
-driver.find_element(By.CSS_SELECTOR,"input[id='contact']").click()
-driver.find_element(By.CSS_SELECTOR,"input[id='contact']").send_keys(config['info']['contact'])
-driver.find_element(By.CSS_SELECTOR,"input[id='phone']").click()
-driver.find_element(By.CSS_SELECTOR,"input[id='phone']").send_keys(config['info']['phone'])
-driver.find_element(By.CSS_SELECTOR,"input[id='email']").click()
-driver.find_element(By.CSS_SELECTOR,"input[id='email']").send_keys(config['info']['email'])
-driver.find_element(By.CSS_SELECTOR,"input[id='city']").click()
-driver.find_element(By.CSS_SELECTOR,"input[id='city']").clear()
-driver.find_element(By.CSS_SELECTOR,"input[id='city']").send_keys(config['info']['city'])
-driver.find_element(By.CSS_SELECTOR,"select[name='Location']").click()
-driver.find_element(By.CSS_SELECTOR,"option[value='Other']").click()
-driver.find_element(By.CSS_SELECTOR,"input[name='Comments']").click()
-driver.find_element(By.CSS_SELECTOR,"input[name='Comments']").send_keys(config['info']['location'])
+
+class SendCalibrationRequest():
+
+	def config_setup(self):
+		file = 'config.ini'
+		self.config = ConfigParser()
+		self.config.read(file)
+
+	def webpage_setup(self):
+		s = Service(self.config['service']['address'])
+		self.driver = webdriver.Chrome(service = s)
+		self.driver.get(webpage)
+
+	def user_login(self):
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='user']").send_keys(self.config['account']['username'])
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='pass']").send_keys(self.config['account']['password'])
+		self.driver.find_element(By.CSS_SELECTOR,"input[type='submit']").click()
+
+	def create_shipper(self):
+		self.driver.find_element(By.XPATH,"(//a[@class='dropdown-toggle'])[2]").click()
+		self.driver.find_element(By.XPATH,"(//a[@title='Create Shipper'])[1]").click()
+		self.driver.find_element(By.XPATH,"(//input[@class='field'])[3]").click()
+
+	def user_input_pause(self):
+		wait = WebDriverWait(self.driver,20)
+		time.sleep(10)
+		wait.until(EC.element_to_be_clickable((By.XPATH,"(//img[@alt='next day pickup'])"))).click()
+
+	def tab1_switch(self):
+		windowsOpened = self.driver.window_handles
+		self.driver.switch_to.window(windowsOpened[1])
+
+	def populate_pickup_request(self):
+		self.driver.find_element(By.CSS_SELECTOR,"select[id='shipTo']").click()
+		self.driver.find_element(By.CSS_SELECTOR,"option[value='2']").click()
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='contact']").click()
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='contact']").send_keys(self.config['info']['contact'])
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='phone']").click()
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='phone']").send_keys(self.config['info']['phone'])
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='email']").click()
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='email']").send_keys(self.config['info']['email'])
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='city']").click()
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='city']").clear()
+		self.driver.find_element(By.CSS_SELECTOR,"input[id='city']").send_keys(self.config['info']['city'])
+		self.driver.find_element(By.CSS_SELECTOR,"select[name='Location']").click()
+		self.driver.find_element(By.CSS_SELECTOR,"option[value='Other']").click()
+		self.driver.find_element(By.CSS_SELECTOR,"input[name='Comments']").click()
+		self.driver.find_element(By.CSS_SELECTOR,"input[name='Comments']").send_keys(self.config['info']['location'])
